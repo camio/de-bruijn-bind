a8f0fd45bcad5a27eb002da71f319d5a00b27686
David Sankel <david@sankelsoftware.com>
2010-09-03 10:57:19 -0400

    Added curry and uncurry examples (seems like G++ 4.5 bugs prevent compilation)

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,14 @@ int minus( int a, int b )
     return a - b;
 }
 
+struct MinusF
+{
+    int operator()( int a, int b ) const
+    {
+        return a - b;
+    }
+} const minusF = MinusF();
+
 int times2( int a )
 {
     return a*2;
@@ -15,6 +23,13 @@ int times3( int a )
 {
     return a*3;
 }
+
+#include <boost/function.hpp>
+boost::function<int (int)> curriedF( int i )
+{
+    return times2;
+}
+
 
 int main()
 {
@@ -46,6 +61,21 @@ int main()
                               )
                          );
 
+    auto curry = abs<1>( abs<1>( abs<1>( app( var<3,1>()
+                                            , var<2,1>()
+                                            , var<1,1>()
+                                            )
+                                       )
+                               )
+                       );
+    auto uncurry = abs<1>( abs<2>( app( app( var<2,1>()
+                                           , var<1,1>()
+                                           )
+                                      , var<1,2>()
+                                      )
+                                 )
+                         );
+
     std::cout << id( "a" )
               << '\n' << always33( "asdf" )
               << '\n' << const_( "asdf" )( 12 )
@@ -53,5 +83,8 @@ int main()
               << '\n' << flip( minus )( 10, 2 )
               << '\n' << compose( times2, times3 )( 4 )
               << '\n' << negFour()
+              //gcc-4.5 barfs without ICE on these cases.
+//              << '\n' << curry(minus)(3)(2)
+//              << '\n' << uncurry(curriedF)(3,2)
               << '\n';
 }
