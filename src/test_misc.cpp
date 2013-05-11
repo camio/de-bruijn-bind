bd9f9b8013404b62a80b891808f887ca213ba22d
David Sankel <david@sankelsoftware.com>
2013-05-10 22:37:41 -0600

    adding missing tdepth test case

--- src/test_misc.cpp
+++ src/test_misc.cpp
@@ -23,3 +23,67 @@ BOOST_AUTO_TEST_CASE( test_dbb_misc )
 
   BOOST_CHECK_EQUAL( negFour( ), -4 );
 }
+
+BOOST_AUTO_TEST_CASE( test_dbb_tdepth )
+{
+  BOOST_MPL_ASSERT(( boost::mpl::equal_to
+                      < tdepth::apply< arg<3,1> >::type
+                      , boost::mpl::int_<3>
+                      >
+                   ));
+  BOOST_MPL_ASSERT(( boost::mpl::equal_to
+                      < tdepth::apply< int >::type
+                      , boost::mpl::int_<0>
+                      >
+                   ));
+  BOOST_MPL_ASSERT(( boost::mpl::equal_to
+                      < tdepth::apply< Abs<1, arg<1,1> >
+                                     >::type
+                      , boost::mpl::int_<0>
+                      >
+                   ));
+  BOOST_MPL_ASSERT(( boost::mpl::equal_to
+                      < tdepth::apply< Abs< 1
+                                          , Abs< 1
+                                               , arg<1,1>
+                                               >
+                                          >
+                                     >::type
+                      , boost::mpl::int_<0>
+                      >
+                   ));
+  BOOST_MPL_ASSERT(( boost::mpl::equal_to
+                      < tdepth::apply< Abs< 1
+                                          , Abs< 1
+                                               , arg<3,1>
+                                               >
+                                          >
+                                     >::type
+                      , boost::mpl::int_<1>
+                      >
+                   ));
+  BOOST_MPL_ASSERT(( boost::mpl::equal_to
+                      < tdepth::apply< App< boost::mpl::void_
+                                          , boost::fusion::vector
+                                             < arg<3,1>
+                                             , arg<5,1>
+                                             >
+                                          >
+                                     >::type
+                      , boost::mpl::int_<5>
+                      >
+                   ));
+  BOOST_MPL_ASSERT(( boost::mpl::equal_to
+                      < tdepth::apply< Abs< 1
+                                          , App< boost::mpl::void_
+                                               , boost::fusion::vector
+                                                  < arg<3,1>
+                                                  , arg<4,1>
+                                                  >
+                                               >
+                                          >
+                                     >::type
+                      , boost::mpl::int_<3>
+                      >
+                   ));
+}
